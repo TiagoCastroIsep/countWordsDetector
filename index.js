70214ae44042ef4947872911b5b973fbbe854e11
TiagoCastroIsep <147203837+TiagoCastroIsep@users.noreply.github.com>
2023-10-17 12:33:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const incrementBtn = document.querySelector(".increment-btn");
 const textToListen = document.querySelector("#text-to-listen");
 const listenBtn = document.querySelector("#listen-btn");
 const stopListeningBtn = document.querySelector("#stop-listening-btn");
+const voiceTranscript = document.getElementById("voice-transcript");
 
 currCounter.value = 1;
 
@@ -21,13 +22,14 @@ const handleListenClick = (e) => {
   window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   const recognition = new SpeechRecognition();
   recognition.continuous = true;
-  recognition.interimResults = false;
+  recognition.interimResults = true;
   recognition.lang = "pt-PT";
 
   const handleRecognition = (e) => {
     const transcript = Array.from(e.results).map(result => result[0]).map(result => result.transcript).join("");
     if (e.results[0].isFinal) {
       console.log(transcript);
+      voiceTranscript.innerHTML = transcript;
       if (transcript.toLowerCase().includes(textToListen.value.toLowerCase().trim())) {
         let pos = transcript.indexOf(textToListen.value.toLowerCase().trim());
 
@@ -42,9 +44,7 @@ const handleListenClick = (e) => {
   recognition.addEventListener("result", handleRecognition);
   recognition.start();
 
-  stopListeningBtn.addEventListener("click", () => {
-    recognition.stop();
-  });
+  stopListeningBtn.addEventListener("click", () => recognition.stop());
 }
 
 listenBtn.addEventListener("click", handleListenClick);
